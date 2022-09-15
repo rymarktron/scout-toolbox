4eab3de38b0c09365b5e13472a44055481f437b2
rymarktron <rymarktron2025@gmail.com>
2022-09-15 00:42:54 -0400

    remove blur

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,37 @@ import Highlight, { defaultProps } from 'prism-react-renderer'
 
 import { Button } from '@/components/Button'
 import { HeroBackground } from '@/components/HeroBackground'
-import blurCyanImage from '@/images/blur-cyan.png'
-import blurIndigoImage from '@/images/blur-indigo.png'
+//import blurCyanImage from '@/images/blur-cyan.png'
+/* <Image
+              className="absolute -mb-56 opacity-50 bottom-full right-full -mr-72"
+              src={blurCyanImage}
+              alt=""
+              width={530}
+              height={530}
+              unoptimized
+              priority
+            />
+
+                <Image
+                className="absolute -top-64 -right-64"
+                src={blurCyanImage}
+                alt=""
+                width={530}
+                height={530}
+                unoptimized
+                priority
+              />
+              <Image
+                className="absolute -bottom-40 -right-44"
+                src={blurIndigoImage}
+                alt=""
+                width={567}
+                height={567}
+                unoptimized
+                priority
+              />
+*/
+//import blurIndigoImage from '@/images/blur-indigo.png'
 
 const codeLanguage = 'javascript'
 const code = `export default {
@@ -38,15 +67,6 @@ export function Hero() {
       <div className="py-16 sm:px-2 lg:relative lg:py-20 lg:px-0">
         <div className="grid items-center max-w-2xl grid-cols-1 px-4 mx-auto gap-y-16 gap-x-8 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
           <div className="relative z-10 md:text-center lg:text-left">
-            <Image
-              className="absolute -mb-56 opacity-50 bottom-full right-full -mr-72"
-              src={blurCyanImage}
-              alt=""
-              width={530}
-              height={530}
-              unoptimized
-              priority
-            />
             <div className="relative">
               <p className="inline text-5xl tracking-tight text-transparent bg-gradient-to-r from-green-200 via-sky-400 to-green-200 bg-clip-text font-display">
                 Welcome to the Scout Toolbox
@@ -68,24 +88,6 @@ export function Hero() {
               <HeroBackground className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 lg:left-0 lg:translate-x-0 lg:translate-y-[-60%]" />
             </div>
             <div className="relative">
-              <Image
-                className="absolute -top-64 -right-64"
-                src={blurCyanImage}
-                alt=""
-                width={530}
-                height={530}
-                unoptimized
-                priority
-              />
-              <Image
-                className="absolute -bottom-40 -right-44"
-                src={blurIndigoImage}
-                alt=""
-                width={567}
-                height={567}
-                unoptimized
-                priority
-              />
               <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10 blur-lg" />
               <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-sky-300 via-sky-300/70 to-blue-300 opacity-10" />
               <div className="relative rounded-2xl bg-[#0A101F]/80 ring-1 ring-white/10 backdrop-blur">
